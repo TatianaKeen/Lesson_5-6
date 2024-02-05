0f018733aa462bdce91ebbf653c86fde7cfa1188
tetiana <tprokopieva@logixits.com>
2024-02-05 13:51:17 -0500

    add error handling; fix formating

--- task3.js
+++ task3.js
@@ -3,13 +3,17 @@ document.addEventListener('DOMContentLoaded', function () {
   const toggleFullscreen = document.getElementById('toggleFullscreen');
 
   toggleFullscreen.addEventListener('click', function () {
-    if (document.fullscreenElement) {
-      document.exitFullscreen()
-      element.style.background = '#dfedaf'
+    try {
+      if (document.fullscreenElement) {
+        document.exitFullscreen();
+        element.style.background = '#dfedaf';
 
-    } else {
-      element.requestFullscreen()
-      element.style.background = '#afe4ed'
+      } else {
+        element.requestFullscreen();
+        element.style.background = '#afe4ed';
+      }
+    } catch (error) {
+      console.error("Toggle Fullscreen Error", error);
     }
   });
 });
